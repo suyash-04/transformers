a3317d79959c674f822c10af339aab2804f338d3
suyash <suyashadhikari04@gmail.com>
2025-06-01 20:22:23 +0545

    Add: validation for model training

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.utils.data import Dataset , DataLoader  , random_split
 from datasets import load_dataset, concatenate_datasets
 import pandas as pd
+import os
 import warnings
 
 from model import build_transformer
@@ -15,8 +16,99 @@ from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from pathlib import Path
+import torchmetrics
 
 from torch.utils.tensorboard import SummaryWriter
+
+
+def greedy_decode( model , source ,source_mask, tokernizer_src, tokenizer_tgt, max_len , device):
+    sos_idx =tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+    
+    encoder_output  = model.encode(source , source_mask)
+    decoder_input = torch.empty(1,1).fill(sos_idx).type_as(source).to(device)
+    
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+        
+        decode_mask = casual_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+        
+        out = model.decode(encoder_output, decoder_input ,decode_mask, source_mask)
+        
+        prob = model.project(out[: ,-1])
+        _, next_word = torch.max(prob , dim=1 )
+        decoder_input = torch.cat(
+            [decoder_input, torch.empty(1,1,).type_as(source).fill_(next_word.item()).to(device)] , dim = 1
+        )
+        if next_word == eos_idx:
+            break
+        
+        return decoder_input.squeeze(0)
+    
+def run_validation(model , validation_ds , tokenizer_src , tokenizer_tgt , max_len , device , print_msg , global_step , writer , num_examples = 2):
+    model.eval()
+    count = 0 
+    source_texts = []
+    expected = []
+    predicted =[]
+    
+    try:
+        # get the console window width
+        with os.popen('stty size', 'r') as console:
+            _, console_width = console.read().split()
+            console_width = int(console_width)
+    except:
+        # If we can't get the console width, use 80 as default
+        console_width = 80
+    with torch.no_grad():
+        for batch in validation_ds:
+            count +=1 
+            encoder_input = batch['encoder_input'].to(device)
+            encoder_mask = batch['encoder_mask'].to(device)
+            
+            assert encoder_input.size(0) ==1, "Batch size must be one"
+            
+            model_out = greedy_decode(model , encoder_input , encoder_mask, tokenizer_src, tokenizer_tgt , max_len , device)
+            
+            source_text = batch['src_text'][0]
+            target_text = batch['target_text'][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+            
+            source_texts.append(source_text)
+            expected.append(target_text)
+            predicted.append(model_out_text)
+            print_msg('-'*console_width)
+            print_msg(f"{f'SOURCE: ':>12}{source_text}")
+            print_msg(f"{f'TARGET: ':>12}{target_text}")
+            print_msg(f"{f'PREDICTED: ':>12}{model_out_text}")
+
+            if count == num_examples:
+                print_msg('-'*console_width)
+                break
+         if writer:
+        # Evaluate the character error rate
+            # Compute the char error rate 
+            metric = torchmetrics.CharErrorRate()
+            cer = metric(predicted, expected)
+            writer.add_scalar('validation cer', cer, global_step)
+            writer.flush()
+
+            # Compute the word error rate
+            metric = torchmetrics.WordErrorRate()
+            wer = metric(predicted, expected)
+            writer.add_scalar('validation wer', wer, global_step)
+            writer.flush()
+
+            # Compute the BLEU metric
+            metric = torchmetrics.BLEUScore()
+            bleu = metric(predicted, expected)
+            writer.add_scalar('validation BLEU', bleu, global_step)
+            writer.flush()   
+                
+            
+            
+
 
 def get_all_sentences(ds, lang):
     for item in ds:
@@ -136,10 +228,11 @@ def train_model(config):
     
     for epoch in range(initial_epoch, config['num_epochs']):
         torch.cuda.empty_cache()
-        model.train()
+        
         
         batch_iterator = tqdm(train_dataloader, desc=f"Processing epoch {epoch : 02d}")
         for batch in batch_iterator:
+            model.train()
             encoder_input = batch['encoder_input'].to(device)
             decoder_input = batch['decoder_input'].to(device)
             encoder_mask = batch['encoder_mask'].to(device)
@@ -160,9 +253,10 @@ def train_model(config):
             optimizer.step()
             optimizer.zero_grad(set_to_none=True)
             global_step += 1
-            
-            model_filename = get_weights_file_path(config, f"{epoch:02d}")
-            torch.save({
+        
+        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)    
+        model_filename = get_weights_file_path(config, f"{epoch:02d}")
+        torch.save({
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
